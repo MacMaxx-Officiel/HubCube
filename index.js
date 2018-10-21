83a619a84bcfc1660ecb4e5f4a8f69d8425cb6aa
MacMaxx-Officiel <44337625+MacMaxx-Officiel@users.noreply.github.com>
2018-10-21 15:43:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const cfg = require('./index.json');
 const token = process.env.token
 const prefix = ("_");
 
@@ -14,8 +15,6 @@ bot.on('guildMemberAdd', member => {
         console.log(`${member.displayName} à rejoind le serveur.`)
     }).catch(console.error)
 });
-
-const ban = require('./kick et ban/ban');
 
 
 bot.on('message', function (message){
